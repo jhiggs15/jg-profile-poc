c07c77078a76a0a3d018529a976421ffec6f2c38
jhiggs15 <jfhiggins98@gmail.com>
2022-07-09 15:56:35 -0400

    Finished array field

--- src/components/Fields/ArrayField.js
+++ src/components/Fields/ArrayField.js
@@ -3,73 +3,96 @@ import { sectionStateHook, treeHook } from '../../util/Atoms';
 import { useRecoilState } from 'recoil';
 import { Input, Button } from 'antd';
 
-const ArraySubItem = ({sectionTitle, title, index, field}) => {
-  const [section, setSection] = useRecoilState(sectionStateHook)
+// TODO setting state may be able to be simplified
+
+const ArraySubItem = ({ sectionTitle, title, index, field }) => {
+  const [section, setSection] = useRecoilState(sectionStateHook);
   const update = (newValue) => {
-    const newSection = {...section}
-    const newSectionItem = {...newSection[sectionTitle]}
-    const newArray = [...newSectionItem[title]]
-    const newArrayItem = newArray[index]
-    newArrayItem[field] = newValue
-    newArray[index] = newArrayItem
-    newSectionItem[title] = newArrayItem
-    newSection[sectionTitle] = newSectionItem
-    setSection(newSection)
-  }
+    const newSection = { ...section };
+    const newSectionItem = { ...newSection[sectionTitle] };
+    const newArray = [...newSectionItem[title]];
+    const newArrayItem = { ...newArray[index], [field]: newValue };
+    newArray[index] = newArrayItem;
+    newSectionItem[title] = newArray;
+    newSection[sectionTitle] = newSectionItem;
+    setSection(newSection);
+  };
 
+  return <Input onChange={(event) => update(event.target.value)} />;
+};
 
-  return (
-    <Input
-      onChange={event => update(event.target.value)}
-   />
-  )
-
-}
-
-const ArrayItem = ({sectionTitle, title, index, fields}) => {
-
+// lets array items be multi valued attributes
+const ArrayItem = ({ sectionTitle, title, index, fields }) => {
   const renderArrayFields = () => {
-    return fields.map(field => <ArraySubItem sectionTitle={sectionTitle} title={title} index={index} field={field} />)
+    return fields.map((field) => (
+      <ArraySubItem
+        sectionTitle={sectionTitle}
+        title={title}
+        index={index}
+        field={field}
+      />
+    ));
+  };
 
-  }
-
-  return (
-    <div>
-      {renderArrayFields()}
-    </div>
-  )
-
-}
+  return <div>{renderArrayFields()}</div>;
+};
 
 // array fields contain array items which could have multiple attributes and this multiple ArraySubItems
 export const ArrayField = ({ sectionTitle, title, templateItem }) => {
   const newTitle = `Array Field ${title}`;
-  const [section, setSection] = useRecoilState(sectionStateHook)
+  const [section, setSection] = useRecoilState(sectionStateHook);
 
   const getLength = () => {
-    if(typeof section[sectionTitle] != "undefined" && typeof section[sectionTitle][title] != "undefined") {
-      const arrayOfItems = section[sectionTitle][title]
-      return arrayOfItems.length
+    if (
+      typeof section[sectionTitle] != 'undefined' &&
+      typeof section[sectionTitle][title] != 'undefined'
+    ) {
+      const arrayOfItems = section[sectionTitle][title];
+      return arrayOfItems.length;
     }
-    return 0
-
-  }
+    return 0;
+  };
 
   const renderArrayItems = () => {
-    if(typeof section[sectionTitle] != "undefined" && typeof section[sectionTitle][title] != "undefined") {
-      const arrayOfItems = section[sectionTitle][title]
-      return arrayOfItems.map((item, index) => <ArrayItem sectionTitle={sectionTitle} title={title} index={index} fields={Object.keys(templateItem)} />)
+    if (
+      typeof section[sectionTitle] != 'undefined' &&
+      typeof section[sectionTitle][title] != 'undefined'
+    ) {
+      const arrayOfItems = section[sectionTitle][title];
+      return arrayOfItems.map((item, index) => (
+        <ArrayItem
+          sectionTitle={sectionTitle}
+          title={title}
+          index={index}
+          fields={Object.keys(templateItem)}
+        />
+      ));
     }
-
-  }
+  };
 
   const addItem = () => {
-    const newSection = {...section}
-    if(typeof section[sectionTitle] == "undefined") newSection[sectionTitle] = {[title] : [templateItem]}
-    else newSection[sectionTitle][title].push(templateItem)
+    const newSection = { ...section };
+    if (typeof section[sectionTitle] == 'undefined')
+      newSection[sectionTitle] = { [title]: [templateItem] };
+    else {
+      const newSectionItem = { ...newSection[sectionTitle] };
+      const newArray = [...newSectionItem[title]];
+      newArray.push(templateItem);
+      newSectionItem[title] = newArray;
+      newSection[sectionTitle] = newSectionItem;
+    }
 
-    setSection(newSection)
+    setSection(newSection);
+  };
 
+  const removeItem = () => {
+    const newSection = { ...section };
+    const newSectionItem = { ...newSection[sectionTitle] };
+    const newArray = [...newSectionItem[title]];
+    newArray.pop(templateItem);
+    newSectionItem[title] = newArray;
+    newSection[sectionTitle] = newSectionItem;
+    setSection(newSection);
   };
 
   return (
@@ -77,7 +100,7 @@ export const ArrayField = ({ sectionTitle, title, templateItem }) => {
       <h1>{newTitle}</h1>
       {renderArrayItems()}
       {getLength() > 0 ? (
-        <Button onClick={}>Remove Last</Button>
+        <Button onClick={removeItem}>Remove Last</Button>
       ) : null}
 
       <Button onClick={addItem}>Create Item</Button>
